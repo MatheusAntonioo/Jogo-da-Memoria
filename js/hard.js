58ebaaccca05aca8d05f840e13a3de999f7e4759
Matheus Antonio <dev.matheus.antonio@outlook.com>
2023-05-30 13:55:08 -0300

    erro de responsividade

--- js/hard.js
+++ js/hard.js
@@ -29,7 +29,7 @@ let cardHTML = "";
 
 images.forEach(img => {
   cardHTML += `
-  <div class="memory-card" data-card="${img}">
+  <div id=hard class="memory-card" data-card="${img}">
     <img class="front-face" src="/img/${img}"/>
     <img class="back-face" src="/img/a-back-escudo.png">
   </div>`;
